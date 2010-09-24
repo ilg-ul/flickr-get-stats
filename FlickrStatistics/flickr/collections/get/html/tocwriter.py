9851d24e1a87206f21e7d3af89e88b314e0c3140
Liviu Ionescu <ilg@livius.net>
2010-09-24 21:18:56 +0000

    files changed to streams
    git-svn-id: http://svn.code.sf.net/p/flickrgetstats/code/trunk/Eclipse@28 d877a8d7-4629-4a7f-8db1-9db580c9df60

--- FlickrStatistics/flickr/collections/get/html/tocwriter.py
+++ FlickrStatistics/flickr/collections/get/html/tocwriter.py
@@ -12,42 +12,42 @@ class TOCWriter(WriterBase):
     classdocs
     '''
 
-    def __init__(self):
+    def __init__(self, sDescription='TOC'):
         '''
         Constructor
         '''
-        super(TOCWriter, self).__init__('TOC')
+        super(TOCWriter, self).__init__(sDescription)
 
     def writeBegin(self):
-        self.fOut.write('<table class="f_toc" summary="Contents"><tr><td><div class="f_toc_title"><h2>Contents</h2></div>\n')
-        self.fOut.write('<ul class="f_toc_coll">\n')
+        self.oOutStream.write(u'<table class="f_toc" summary="Contents"><tr><td><div class="f_toc_title"><h2>Contents</h2></div>\n')
+        self.oOutStream.write(u'<ul class="f_toc_coll">\n')
         return
     
     def writeEnd(self):
-        self.fOut.write('</ul>\n')
-        self.fOut.write('</td></tr></table>\n')
+        self.oOutStream.write(u'</ul>\n')
+        self.oOutStream.write(u'</td></tr></table>\n')
         return
     
     def writeCollectionBegin(self):
         if self.bVerbose:
             print '%s%s Collection "%s"' % (self.sIndent, self.sHierarchicalDepth, self.sCollectionTitle)
-        self.fOut.write('  <li class="f_toc_lvl%d"><a href="#C%s"><span class="f_toc_num">%s</span><span class="f_toc_txt">%s</span></a>\n' % (self.nDepth, self.sCollectionID, self.sHierarchicalDepth, self.sCollectionTitle))
+        self.oOutStream.write(u'  <li class="f_toc_lvl%d"><a href="#C%s"><span class="f_toc_num">%s</span><span class="f_toc_txt">%s</span></a>\n' % (self.nDepth, self.sCollectionID, self.sHierarchicalDepth, self.sCollectionTitle))
         return
 
     def writePhotosetBegin(self):
         if self.bVerbose:
             print '%s%s Set "%s"' % (self.sIndent, self.sHierarchicalDepth, self.sPhotosetTitle)
-        self.fOut.write('  <li class="f_toc_lvl%d"><a href="#A%s"><span class="f_toc_num">%s</span><span class="f_toc_txt">%s</span></a>\n' % (self.nDepth, self.sPhotosetID, self.sHierarchicalDepth, self.sPhotosetTitle))
+        self.oOutStream.write(u'  <li class="f_toc_lvl%d"><a href="#A%s"><span class="f_toc_num">%s</span><span class="f_toc_txt">%s</span></a>\n' % (self.nDepth, self.sPhotosetID, self.sHierarchicalDepth, self.sPhotosetTitle))
         return
 
     def writeEmbeddedBegin(self):
-        self.fOut.write('<ul class="f_coll">\n')
+        self.oOutStream.write(u'<ul class="f_coll">\n')
         return
 
     def writeEmbeddedEnd(self):
-        self.fOut.write('</ul>\n')
+        self.oOutStream.write(u'</ul>\n')
         return
 
     def writeCollectionEnd(self):
-        self.fOut.write('</li>\n')
+        self.oOutStream.write(u'</li>\n')
         return
