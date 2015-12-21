0dd2b4da792d03ba0b98b1cbe8ec7b4eb6939e15
Liviu Ionescu <ilg@livius.net>
2015-12-21 19:33:23 +0200

    clean authenticate messages

--- fkgs-scripts/py-fkgs/src/ilg/flickr/authenticate/__main__.py
+++ fkgs-scripts/py-fkgs/src/ilg/flickr/authenticate/__main__.py
@@ -1,63 +1,14 @@
 """
 Usage:
-    python -m ilg.flickr.statistics.get.csv [options]
-
-Options:
-    -f "FOLDER", --folder="FOLDER"
-        destination folder where CSV files are stored; 
-        default is '$HOME/Documents/Flickr/Statistics'
-
-    -d "DAY", --day="DAY"
-        the day to get statistics for; the format is 
-        quite strict, and should look like "2010-06-30"
-     
-    -v, --verbose
-        print progress output
-
-    -V, --version
-        print program version
-
-    -h, --help
-        print this message
+    python -m ilg.flickr.authenticate
 
 Purpose:
-    Retrieve daily CSV statistic files for a Flickr account.
-
----
-
-This program will generate one CSV file per day. The file format is 
-exactly the same as used by Flickr in the past, when they provided 
-aggregated statistics.
-
-The output files contain the date in the file name, like:
-    2010-06-30-flickr.csv
-    
-The files already present in the destination folder are not 
-fetched again.
-
-The program will try to go back in time and fetch as much 
-statistics as possible, about one month (according to Flickr, 
-at least 28 days are stored). 
-
-If the --day option is used, only one day of statistics is fetched.
-
-The authentication is done only once, and the Flickr authentication 
-token is stored in the user home directory.
+    Authenticate and store the authentication token.
  
 """
 
 import flickrapi
-import webbrowser
 import sys
-import time
-import getopt
-import os
-import requests
-#import ssl
-
-from ilg.flickr.statistics.get.aggregate import Aggregate
-from ilg.flickr.statistics.get.csv.writer import Writer
-
 
 '''
 The below key identifies this application only, for accessing Flickr.
