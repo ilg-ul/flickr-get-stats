310066b1af91d4988380b7b6630410f54c205ca3
Liviu Ionescu <ilg@livius.net>
2017-09-23 13:58:11 +0300

    writer.py:fix utf-8 in sRef

--- python/flickr-get-stats/src/ilg/flickr/statistics/get/csv/writer.py
+++ python/flickr-get-stats/src/ilg/flickr/statistics/get/csv/writer.py
@@ -21,6 +21,6 @@ class Writer(WriterBase):
     def flush(self):
         sUrl = self.getKindFullUrl();            
         sRef = self.sFullReferrerUrl
-        self.oOutStream.write('%s,%s,"%s","%s","%s",%d\n' % (self.sDate, sUrl, sRef, self.toUTF8(self.sReferrerDomain), self.toUTF8(self.sSearchTerm), self.nReferrerCount))
+        self.oOutStream.write('%s,%s,"%s","%s","%s",%d\n' % (self.sDate, sUrl, self.toUTF8(sRef), self.toUTF8(self.sReferrerDomain), self.toUTF8(self.sSearchTerm), self.nReferrerCount))
 
 
